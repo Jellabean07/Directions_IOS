e38fe02868ccedd7d26bec9956d80ed397497607
Jellabean <mjkiol12@naver.com>
2017-12-06 15:50:51 +0900

    first commit

---
+++ Directions/controller/MainVC.swift
@@ -0,0 +1,9 @@
+//
+//  MainVC.swift
+//  Directions
+//
+//  Created by CSC-PC on 2017. 12. 5..
+//  Copyright © 2017년 myWay. All rights reserved.
+//
+
+import Foundation
---
+++ Directions/controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Directions
+//
+//  Created by CSC-PC on 2017. 12. 4..
+//  Copyright © 2017년 myWay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
